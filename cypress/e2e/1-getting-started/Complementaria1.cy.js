9a05afe0fa2121c7c4596f3159e3151ec5ff070f
Francisco Cabrera <francabre97@gmail.com>
2023-12-19 01:20:48 -0300

    Creacion de complementaria 2

--- cypress/e2e/1-getting-started/Complementaria1.cy.js
+++ cypress/e2e/1-getting-started/Complementaria1.cy.js
@@ -3,7 +3,7 @@
     const numero = Math.floor(Math.random() * 1000)
     it('Actividadcomplementeria 1', () =>{
         cy.visit('https://pushing-front.vercel.app/');
-        cy.get('#user').type('francisco');
+        cy.get('#user').type(`francisco${numero}`);
         cy.get('#pass').type('123456!');
         cy.get('[value=Male]').check({force:true});
         cy.get('#day').select('31');
--- cypress/e2e/1-getting-started/Complementaria2.cy.js
+++ cypress/e2e/1-getting-started/Complementaria2.cy.js
@@ -2,15 +2,12 @@ describe("Actividad complementaria", () =>{
     const numero = Math.floor(Math.random() * 1000)
     it('Actividadcomplementeria 1', () =>{
         cy.visit('https://pushing-front.vercel.app/');
-        cy.get('#user').type('francisco');
-        cy.get('#pass').type('123456!');
-        cy.get('[value=Male]').check({force:true});
-        cy.get('#day').select('31');
-        cy.get('#month').select(7);
-        cy.get('#year').select('1997');
+        cy.get('#user').type(`francisco${numero}`);
+        cy.get('[name^=pa]').type('123456!');//prefijo sub-string matches
+        cy.get('[value=Male]').check({force:true});//este es por atributo
+        cy.get('select#day').select("31"); // valor 31
+        cy.get('[id$=nth]').select(10);//sufijo sub-string matches & array 10
+        cy.get('#year').select('1998');
         cy.get('#submitForm').click();
-        //crear un test que permita registrarse en la pagina!
-        //Recorda usar npx cypress open para abrir la interfaz de usuario
-        //Para seleccionar el genero utiliza el siguiente selector cy.get("[value='Male']") //Female // recuerden el force true
     });
 });
