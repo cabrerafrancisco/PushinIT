b621dbc3f65b81273a167b0a38b45b551221e362
Francisco Cabrera <francabre97@gmail.com>
2024-01-09 00:36:10 -0300

    Actividad Comple 3

--- cypress/e2e/1-getting-started/Complementaria1.cy.js
+++ cypress/e2e/1-getting-started/Complementaria1.cy.js
@@ -10,13 +10,5 @@
         cy.get('#month').select(7);
         cy.get('#year').select('1997');
         cy.get('#submitForm').click();
-        //crear un test que permita registrarse en la pagina!
-        //Recorda usar npx cypress open para abrir la interfaz de usuario
-        //Para seleccionar el genero utiliza el siguiente selector cy.get("[value='Male']") //Female // recuerden el force true
     });
 });
-
-//Si tenes ganas de seguir practicando te propongo que con lo aprendido en clase te registres al sistema con los siguientes datos 
-// usuario: 'pushingit'
-// contraseña '123456!'
-//Para ir al login tienen que hacer doble click en el elemento 'incia sesion'
---
+++ cypress/e2e/1-getting-started/Complementaria3.cy.js
@@ -0,0 +1,23 @@
+describe("Complementaria 3", () => {
+    it('ToDo Lists', () => {
+        cy.visit("");
+        cy.xpath("//div//form//p//span").dblclick();
+        cy.xpath("//input[@id='user']").type("pushingit");
+        cy.xpath("//div//form//div//input[@name='pass']").type('123456!');
+        cy.xpath("//div//form//button[@type='submit']").click();
+        cy.wait(1000);
+        cy.xpath("//div//p//a[@id='todolistlink']").click();
+        cy.xpath("//ancestor::div//descendant::input[@id='task']").type("tarea 1")
+        cy.wait(1000);
+        cy.xpath("//*[@id='sendTask']").click()
+        cy.xpath("/html/body/div[1]/div/div[2]/ul/div/div[1]/li/div").click()
+
+    });
+
+
+
+
+
+
+
+})
